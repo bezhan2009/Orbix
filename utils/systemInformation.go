92f5540da67c632de72379bf058410abc14caa40
bezhan2009 <karimovbezan0@gmail.com>
2024-06-05 15:52:07 +0300

    version 0.03

--- utils/systemInformation.go
+++ utils/systemInformation.go
@@ -3,6 +3,6 @@ package utils
 import "fmt"
 
 func SystemInformation() {
-	fmt.Println("GoCmd [Version Beta 0.03]")
+	fmt.Println("GoCmd [Version 0.03]")
 	fmt.Println("(c) Учебный проект Алиф Академия (Alif Academy), 2024. Все права защищены")
 }
