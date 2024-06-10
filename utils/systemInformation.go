59d826cf4b916421c4d08c2b8f6363cb8d181464
bezhan2009 <karimovbezan0@gmail.com>
2024-06-10 13:28:23 +0300

    release version 0.48

--- utils/systemInformation.go
+++ utils/systemInformation.go
@@ -3,6 +3,6 @@ package utils
 import "fmt"
 
 func SystemInformation() {
-	fmt.Println("ORPXI [Version 0.31]")
+	fmt.Println("ORPXI [Version 0.48]")
 	fmt.Println("(c) Optimized Resource Planning and eXecution Interface (ORPXI), 2024. Все права защищены")
 }
