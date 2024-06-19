719066a0a47a1f66e0da6772ce25fd0aca8a8e23
bezhan2009 <karimovbezan0@gmail.com>
2024-06-19 17:21:20 +0300

    fix fn systeminformation

--- utils/systemInformation.go
+++ utils/systemInformation.go
@@ -10,5 +10,5 @@ func SystemInformation() {
 	editCMD.SayOrbix()
 	magenta := color.New(color.FgMagenta, color.Bold).SprintFunc()
 	fmt.Printf("%s\n", magenta("Orbix [Версия 0.94]"))
-	fmt.Printf("%s\n", magenta("Orbix Software, 2024."))
+	fmt.Printf("%s\n", magenta("(S) Orbix Software, 2024. No license yet."))
 }
