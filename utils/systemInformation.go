b45d175c39d66426e0d279c268e24dc77a8f7992
bezhan2009 <karimovbezan0@gmail.com>
2024-06-19 17:04:07 +0300

    version 0.94

--- utils/systemInformation.go
+++ utils/systemInformation.go
@@ -9,6 +9,6 @@ import (
 func SystemInformation() {
 	editCMD.StartEditing()
 	magenta := color.New(color.FgMagenta, color.Bold).SprintFunc()
-	fmt.Printf("%s\n", magenta("src [Версия 0.74]"))
+	fmt.Printf("%s\n", magenta("src [Версия 0.94]"))
 	fmt.Printf("%s\n", magenta("(c) src Software, 2024. Все права защищены."))
 }
