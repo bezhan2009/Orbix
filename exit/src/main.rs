0bb42cf7a2c46c886de576ad29e9ba142e82a7fb
bezhan2009 <karimovbezan0@gmail.com>
2024-06-22 18:54:09 +0300

    reft: .rs files

--- exit/src/main.rs
+++ exit/src/main.rs
@@ -1,7 +1,22 @@
 use std::process::{Command, exit};
+use std::fs;
 
 fn main() {
+    let file_path = "C:/Users/Admin/MyCMD/goCMD/activeUser.txt";
+
     println!("Executing exit command...");
+
+    // Проверяем существование файла activeUser.txt
+    if fs::metadata(&file_path).is_ok() {
+        // Удаляем файл activeUser.txt
+        if let Err(e) = fs::remove_file(&file_path) {
+            eprintln!("Failed to delete file '{}': {}", file_path, e);
+        } else {
+            println!("File '{}' deleted successfully.", file_path);
+        }
+    } else {
+        println!("File '{}' does not exist.", file_path);
+    }
 
     // Выполнение команды `cmd /C exit` для завершения командной оболочки
     let status = Command::new("cmd")
