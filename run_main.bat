726c042c3f8310fbec1eaf1ee9bf17ec752e635f
bezhan2009 <karimovbezan0@gmail.com>
2024-06-22 15:29:25 +0300

    fix run_main.bat

--- run_main.bat
+++ run_main.bat
@@ -3,9 +3,16 @@
 rem Set paths to Go and Rust files
 set MAIN_GO_FILE=main.go
 set MAIN_RUST_FILE=init\src\main.rs
+set ACTIVE_USER_FILE=activeUser.txt
 
 rem Change directory to the root of the project
 cd /d "%~dp0"
+
+rem Check if activeUser.txt exists and exit if it does
+if exist "%ACTIVE_USER_FILE%" (
+    echo Файл "%ACTIVE_USER_FILE%" существует. Программа завершена.
+    exit /b 1
+)
 
 rem Build Rust project
 rustc "%MAIN_RUST_FILE%"
@@ -18,4 +25,3 @@ pause
 
 rem Build Go project
 go run "%MAIN_GO_FILE%"
-
