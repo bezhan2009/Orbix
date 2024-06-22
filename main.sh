b7704ee072471627f8314807af4fc05e47982fcb
bezhan2009 <karimovbezan0@gmail.com>
2024-06-22 18:50:54 +0300

    fix run_main.bat and main.sh

--- main.sh
+++ main.sh
@@ -5,9 +5,14 @@ ROOT_DIR="C:/Users/Admin/MyCMD/goCMD"
 MAIN_GO_FILE="$ROOT_DIR/main.go"
 MAIN_RUST_FILE="$ROOT_DIR/init/src/main.rs"
 ACTIVE_USER_FILE="$ROOT_DIR/activeUser.txt"
+IS_RUN_FILE="$ROOT_DIR/isRun.txt"
 
 # Переходим в корневую директорию проекта
 cd "$ROOT_DIR" || exit
+
+# Создаем и записываем в isRun.txt
+echo "true" > "$IS_RUN_FILE"
+echo "Файл \"$IS_RUN_FILE\" создан и записано значение true."
 
 # Проверяем существование файла activeUser.txt и удаляем его, если он есть
 if [ -f "$ACTIVE_USER_FILE" ]; then
--- run_main.bat
+++ run_main.bat
@@ -4,9 +4,14 @@ rem Set paths to Go and Rust files
 set MAIN_GO_FILE=main.go
 set MAIN_RUST_FILE=init\src\main.rs
 set ACTIVE_USER_FILE=activeUser.txt
+set IS_RUN_FILE=isRun.txt
 
 rem Change directory to the root of the project
 cd /d "%~dp0"
+
+rem Create and write to isRun.txt
+echo true > "%IS_RUN_FILE%"
+echo Файл "%IS_RUN_FILE%" создан и записано значение true.
 
 rem Check if activeUser.txt exists and exit if it does
 rem Проверяем существование файла activeUser.txt
