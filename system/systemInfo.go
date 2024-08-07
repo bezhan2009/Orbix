d2b91acce20c83b5c370a8c3b6eb895d413042bc
bezhan2009 <karimovbezan0@gmail.com>
2024-08-07 12:17:10 +0300

    version 1.0.10

--- system/systemInfo.go
+++ system/systemInfo.go
@@ -3,7 +3,7 @@ package system
 import "runtime"
 
 const (
-	Version         = "1.0.09"
+	Version         = "1.0.10"
 	License         = "MIT"
 	SystemName      = "Orbix"
 	OperationSystem = runtime.GOOS
