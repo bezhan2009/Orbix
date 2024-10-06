320377eee363ebb53ac8d0e7c6a1990c5382e308
bezhan2009 <karimovbezan0@gmail.com>
2024-10-06 17:06:33 +0300

    [feat] added algorithms for linux in CmdDir and CmdUser

--- cmd/dirInfo/getDirInfo.go
+++ cmd/dirInfo/getDirInfo.go
@@ -1,6 +1,15 @@
 package dirInfo
 
+import (
+	"goCmd/system"
+	"os"
+)
+
 func CmdDir(dir string) string {
+	if system.OperationSystem != "windows" {
+		return dir
+	}
+
 	var count uint16
 	var dirC string
 
@@ -19,22 +28,26 @@ func CmdDir(dir string) string {
 }
 
 func CmdUser(dir string) string {
-	var count uint16
-	var user string
+	if system.OperationSystem == "linux" {
+		// В Linux просто возвращаем текущего пользователя
+		return os.Getenv("USER")
+	} else {
+		var count uint16
+		var user string
 
-	for i := 0; i < len(dir); i++ {
+		for i := 0; i < len(dir); i++ {
+			if dir[i] == '\\' {
+				count++
+				continue
+			}
 
-		if dir[i] == '\\' {
-			count += 1
-			continue
+			if count > 1 && count < 3 {
+				user += string(dir[i])
+			}
 		}
 
-		if count > 1 && count < 3 {
-			user += string(dir[i])
-		}
+		return user
 	}
-
-	return user
 }
 
 func CMDGetDirWithOutApp(dir string) string {
