66ee45df18233ce2a0d283f9f356c6d0ff61ff96
bezhan2009 <karimovbezan0@gmail.com>
2024-10-19 10:45:56 +0300

    version 1.8.0

--- system/system_info.go
+++ system/system_info.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	Version         = "1.7.6"
+	Version         = "1.8.0"
 	License         = "MIT"
 	SystemName      = "Orbix"
 	OperationSystem = runtime.GOOS
--- utils/is_exists.go
+++ utils/is_exists.go
@@ -4,5 +4,12 @@ import "os"
 
 func IsExists(name string) (*os.File, error) {
 	f, err := os.Open(name)
+	defer func(f *os.File) {
+		err = f.Close()
+		if err != nil {
+			return
+		}
+	}(f)
+
 	return f, err
 }
